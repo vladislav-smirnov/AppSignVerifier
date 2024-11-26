5a33a2c5131c27483d9911ddb9c99172926686a6
Vladislav Smirnov <sivdead@gmail.com>
2024-11-26 23:45:28 +0100

    Added pom.xml

--- appsignverifier/build.gradle.kts
+++ appsignverifier/build.gradle.kts
@@ -1,7 +1,14 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+
+    id("com.vanniktech.maven.publish") version "0.30.0"
 }
+
+
+val aarVersion: String? = gradleLocalProperties(rootDir, providers).getProperty("VERSION_NAME")
 
 android {
     namespace = "io.github.airdaydreamers.appsignverifier"
@@ -32,11 +39,53 @@ android {
     }
 }
 
-dependencies {
+//maven
+//publishReleasePublicationToMavenCentralRepository
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "io.github.airdaydreamers.appsignverifier"
+            artifactId = "appsignverifier"
 
+            version = aarVersion ?: "0.8.2-alpha"
+
+            pom {
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+
+                developers {
+                    name = "appsignverifier"
+                    description =
+                        "App Signature Verifier is a utility library for Android that provides tools to verify various properties of applications"
+                    url = "https://github.com/vladislav-smirnov/AppSignVerifier"
+
+                    developer {
+                        id = "vladislavsmirnov"
+                        name = "Vladislav Smirnov"
+                        email = "sivdead@gmail.com"
+                    }
+                }
+
+                scm {
+                    connection = "scm:git:git://github.com/vladislav-smirnov/AppSignVerifier.git"
+                    developerConnection =
+                        "scm:git:ssh://github.com:vladislav-smirnov/AppSignVerifier.git"
+                    url = "https://github.com/vladislav-smirnov/AppSignVerifier"
+                }
+            }
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
+dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -49,6 +98,6 @@ dependencies {
     testImplementation(libs.mockito.kotlin)
     testImplementation(libs.mockito.inline)
 
-    testImplementation (libs.kotlin.test.junit)
-    testImplementation (libs.byte.buddy)
+    testImplementation(libs.kotlin.test.junit)
+    testImplementation(libs.byte.buddy)
 }
