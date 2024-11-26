797da4c3a460d08ee307e8953b6c52771fb057b6
Vladislav Smirnov <sivdead@gmail.com>
2024-11-26 23:45:28 +0100

    Added pom.xml

--- appsignverifier/build.gradle.kts
+++ appsignverifier/build.gradle.kts
@@ -35,8 +35,7 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+   // implementation(libs.androidx.appcompat)
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
